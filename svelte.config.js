2be7948757b00533485370ce94791a28fb4d5acc
YukkuriPannda <100471518+YukkuriPannda@users.noreply.github.com>
2024-12-17 08:51:52 +0000

    cloud flare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { mdsvex } from 'mdsvex';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			'contentlayer/generated':'./.contentlayer/generated'
+			'contentlayer/generated': './.contentlayer/generated'
 		},
 		prerender: {
 			handleHttpError: 'fail'
